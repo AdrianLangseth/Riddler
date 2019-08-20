28055fb75b9fa8a84b8fe155508f38716523185a
Adrian <ad.langseth@gmail.com>
2019-08-20 23:59:26 +0200

    Spelling bee completed

--- 2019.03.29 Spelling Bee/Spelling Bee.py
+++ 2019.03.29 Spelling Bee/Spelling Bee.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 
 class SpellingBeeContenstant():
     _knowledge = 0
-    _name = "You Fucked Up"
 
     def __init__(self, percentage_knowledge):
         self._knowledge = percentage_knowledge
@@ -16,19 +15,22 @@ class SpellingBeeContenstant():
         return str(self._knowledge)
 
 
-def setup_spelling_bee():
+def setup_spelling_bee(incrementation_bool : bool):
     players = []
-    for i in range(90,100):
-        players.append(SpellingBeeContenstant(i))
+    for i in range(0, 10):
+        if incrementation_bool:
+            players.append(SpellingBeeContenstant(90 + i))
+        else :
+            players.append(SpellingBeeContenstant(99 - i))
     return players
 
 
 def test_player(player : SpellingBeeContenstant):
-    return player.get_knowledge() >= r.randint(1,100)
+    return player.get_knowledge() >= r.randint(1, 100)
 
 
-def run_spelling_bee():
-    players = setup_spelling_bee()
+def run_spelling_bee(increment_bool):
+    players = setup_spelling_bee(increment_bool)
 
     while True:
         for player in players:
@@ -38,10 +40,10 @@ def run_spelling_bee():
                 return players[0]
 
 
-def simulate_several_bees(amount):
+def simulate_several_bees(amount, incrementation_bool):
     wincount = [0 for i in range(10)]
     for i in range(amount):
-        wincount[run_spelling_bee().get_knowledge() - 90] += 1
+        wincount[run_spelling_bee(incrementation_bool).get_knowledge() - 90] += 1
     return [x / amount for x in wincount]
 
 
@@ -54,4 +56,4 @@ def graph_wins(win_count_list : list):
     plt.title('Win Share by incrementing order')
     plt.show()
 
-graph_wins(simulate_several_bees(10000))
+graph_wins(simulate_several_bees(10000, False))
